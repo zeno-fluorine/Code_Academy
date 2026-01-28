59557b42846c8291754cf490f264ecbe86bc4505
zeno-fluorine <80643634+zeno-fluorine@users.noreply.github.com>
2026-01-28 13:36:16 -0700

    Create Casting_Types.c

---
+++ Learn_C_Variables_And_Operators/Casting_Types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  
+  double testScore = 95.7;
+  int displayScore = 0;
+
+  displayScore = (int)testScore;
+
+  // No need to change below here
+  printf("Great work, you got a %d%% on your test\n", displayScore);
+
+}
