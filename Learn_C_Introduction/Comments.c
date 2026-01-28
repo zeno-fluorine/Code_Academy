14034e46014dee1532d0f1b8341ba6fe237a34be
zeno-fluorine <80643634+zeno-fluorine@users.noreply.github.com>
2026-01-28 11:07:38 -0700

    Create Comments.c

---
+++ Learn_C_Introduction/Comments.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  // This is true if you are not alergic to chocolate
+  printf("Chocolate chip cookies are way better than oatmeal rasin cookies.\n");
+
+  /* 
+    I love reviewing C!
+  */
+  printf("Learning the C programing language is an exciting adventure!\n");
+}
