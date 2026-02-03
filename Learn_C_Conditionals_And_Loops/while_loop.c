9beb088e5e1657f2ba2ab730272163689b6bd127
zeno-fluorine <80643634+zeno-fluorine@users.noreply.github.com>
2026-02-03 10:29:47 -0700

    Create while_loop.c

---
+++ Learn_C_Conditionals_And_Loops/while_loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+  int i = 0;
+  int j = 9;
+  int square = 0;
+
+  while(i < 10){
+    printf("%d\t%d\n", i, i * i);
+    i++;
+  }
+
+while(j > -1){
+    printf("%d\t%d\n", j, j * j);
+    j--;
+  }
+
+}
