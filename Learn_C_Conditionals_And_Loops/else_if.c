2315542f38cb05a4ec4af2e837a13c29f9f7a62e
zeno-fluorine <80643634+zeno-fluorine@users.noreply.github.com>
2026-02-03 09:57:27 -0700

    Create else_if.c

---
+++ Learn_C_Conditionals_And_Loops/else_if.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+  double ph = 7;
+
+  if (ph > 7){
+    printf("Basic");
+  }
+  else if (ph < 7){
+    printf("Acidic");
+  }
+  else{
+    printf("Neutral");
+  }
+
+
+}
