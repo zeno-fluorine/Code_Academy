6413d2095d6a22c351c1310bdd440ddb61763f41
zeno-fluorine <80643634+zeno-fluorine@users.noreply.github.com>
2026-02-03 09:47:21 -0700

    Create else_clause.c

---
+++ Learn_C_Conditionals_And_Loops/else_clause.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+  int grade1 = 59;
+  int grade2 = 90;
+
+  if (grade1 > 60) {
+    printf("Pass\n");
+  } else {
+    printf("Fail\n");
+  }
+
+  if (grade2 > 60){
+    printf("Pass\n");
+  }else{
+    printf("Fail\n");
+  }
+  
+
+}
